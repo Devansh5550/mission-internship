55568418c7f65277aa14c80d6f4de5409af8d0a7
Devansh5550 <adikum4455@gmail.com>
2025-12-29 22:37:41 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,19 @@
+a = int(input("ENTER FIRST NUMBER ="))
+b = int(input("ENTER SECOND NUMBER ="))
+
+print("SUM = ", a+b)
+print("differnce = ", a-b)
+
+print("product = ", a*b)
+
+print("division = ", a/b)
+
+
+name = input("ENTER STUDENTS NAME =")
+roll = input("ENTER ROLL NO =")
+marks = float(input("ENTER MARKS SCORED ="))
+
+print("\n  STUDENT DATA    ")
+print("NAME:", name)
+print("ROLL-NO:", roll)
+print("MARKS SCORED", marks)
